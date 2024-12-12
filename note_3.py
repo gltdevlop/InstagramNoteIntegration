12937b74a8317e8250dff0ece054b8384473d28d
gltdevlop <99366409+gltdevlop@users.noreply.github.com>
2024-12-12 14:26:37 +0100

    working

--- note_3.py
+++ note_3.py
@@ -10,7 +10,7 @@ user_id = cl.user_id_from_username(ACCOUNT_USERNAME)
 medias = cl.user_medias(user_id, 20)
 
 def send_note(note, audience):
-   cr_note = cl.create_note(note, audience)
+    cl.create_note(note, audience)
 
 def del_note():
     note = cl.create_note("note", 1)
